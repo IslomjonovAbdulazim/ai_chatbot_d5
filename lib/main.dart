890b62d5fb99e616d95ee657b8f4dcd11d6887d2
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-17 18:38:59 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     DevicePreview(
-      enabled: false,
+      enabled: true,
       builder: (context) => AiChatbotApp(),
     ),
   );
