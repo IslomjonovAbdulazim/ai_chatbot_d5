b738b3cb29b38b68250ed7968f9e2eb5b8558614
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-21 17:40:22 +0500

    imp

--- lib/models/auth_models.dart
+++ lib/models/auth_models.dart
@@ -1,3 +1,21 @@
-class AuthModel {
+class AuthRequestModel {
+  late String token;
 
+  AuthRequestModel({required this.token});
+
+  Map toJson() => {
+    "token": token,
+  };
 }
+
+class AuthResponseModel {
+  late String accessToken;
+
+  AuthResponseModel.fromJson(Map json) {
+    accessToken = json["access_token"];
+  }
+}
+
+
+
+
---
+++ lib/utils/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const baseUrl = "https://islomjonovabdulazim-ai-chatbot-backend-9834.twc1.net";
+
+
+}
