cc4f41505608feaf361728de2c1269de03137318
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-21 18:42:43 +0500

    imp

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -5,6 +5,7 @@ import 'package:ai_chatbot_d5/utils/api_constants.dart';
 import 'package:ai_chatbot_d5/utils/google_service.dart';
 import 'package:ai_chatbot_d5/widgets/snackbar_widget.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider {
   static Future<void> signIn() async {
@@ -17,16 +18,35 @@ class AuthProvider {
       final response = await http.post(
         uri,
         headers: {
-          "Content-Type":"application/json",
+          "Content-Type": "application/json",
         },
         body: jsonEncode(model.toJson()),
       );
       final body = jsonDecode(response.body);
       if (response.statusCode == 200) {
+        final data = AuthResponseModel.fromJson(body);
+        final db = await SharedPreferences.getInstance();
+        await db.setString("token", data.accessToken);
+        SnackbarWidget.success(
+          "Successfully Logged In",
+          "Welcome back! Ready to start chatting.",
+        );
 
+      } else if (response.statusCode == 400) {
+        SnackbarWidget.error(
+          "Error with your Google account",
+          body["detail"] ?? "Invalid Google Token",
+        );
       } else {
-
+        SnackbarWidget.error(
+          "Something went wrong",
+          body["detail"] ?? "No Detail",
+        );
       }
     }
+  }
+
+  static Future<bool> navigate() async {
+
   }
 }
