7d2b613e16d4c5cca47970367cbe27482205a1b8
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-06-24 17:31:01 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'pages/auth/splash_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0LXVzZXItMTIzIiwiZXhwIjoxNzUzMzU5NDM3LCJpYXQiOjE3NTA3Njc0Mzd9.JoFPZ1qT9jbG-Vj2rQ8eb-AFXvtt0pHVj-4bie1505c";
+  final db = await SharedPreferences.getInstance();
+  await db.setString("token", token);
   runApp(
     DevicePreview(
       enabled: false,
