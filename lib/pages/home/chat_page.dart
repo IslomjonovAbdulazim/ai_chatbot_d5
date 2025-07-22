881914e82ca77e2a76c44f0503447dac4238288b
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-22 17:30:25 +0500

    imp

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:ai_chatbot_d5/models/message_model.dart';
 import 'package:ai_chatbot_d5/providers/message_provider.dart';
+import 'package:ai_chatbot_d5/widgets/snackbar_widget.dart';
 import 'package:chat_bubbles/bubbles/bubble_special_three.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,7 @@ class _ChatPageState extends State<ChatPage> {
   final controller = TextEditingController();
   final focus = FocusNode();
   bool responding = false;
+  final scroll = ScrollController();
 
   @override
   void initState() {
@@ -34,6 +36,32 @@ class _ChatPageState extends State<ChatPage> {
     setState(() {});
     messages = await MessageProvider.getAllMessages(widget.id);
     isLoading = false;
+    setState(() {});
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (scroll.hasClients) {
+        scroll.jumpTo(scroll.position.maxScrollExtent);
+      }
+    });
+  }
+
+  void send() async {
+    responding = true;
+    setState(() {});
+    final text = controller.text.trim();
+    controller.clear();
+    if (text.length <= 0 || text.length >= 250) {
+      SnackbarWidget.error("Too Large or Empty", "");
+    } else {
+      await MessageProvider.sendMessage(text, widget.id);
+      messages = await MessageProvider.getAllMessages(widget.id);
+      setState(() {});
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (scroll.hasClients) {
+          scroll.jumpTo(scroll.position.maxScrollExtent);
+        }
+      });
+    }
+    responding = false;
     setState(() {});
   }
 
@@ -62,25 +90,83 @@ class _ChatPageState extends State<ChatPage> {
         ),
       ),
       body: SafeArea(
-        child: ListView.builder(
-          reverse: true,
-          itemCount: messages.length,
-          itemBuilder: (context, index) {
-            final model = messages[index];
-            return BubbleSpecialThree(
-              text: model.content,
-              color: Color(
-                model.role == "user" ? 0xff292929 : 0xffD71E1E,
+        child: isLoading
+            ? Center(child: CircularProgressIndicator(color: Colors.white))
+            : Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      controller: scroll,
+                      itemCount: messages.length,
+                      itemBuilder: (context, index) {
+                        final model = messages[index];
+                        return BubbleSpecialThree(
+                          text: model.content,
+                          color: Color(
+                            model.role == "user" ? 0xff292929 : 0xffD71E1E,
+                          ),
+                          textStyle: GoogleFonts.inter(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          isSender: model.role == "user",
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: controller,
+                            focusNode: focus,
+                            onTapOutside: (value) => focus.unfocus(),
+                            style: GoogleFonts.inter(
+                              color: Colors.white,
+                            ),
+                            autocorrect: false,
+                            keyboardAppearance: Brightness.dark,
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Color(0xff292929),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide.none,
+                                ),
+                                hintText: "Ask anything...",
+                                hintStyle: GoogleFonts.inter(
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ),
+                        SizedBox(width: 8),
+                        responding
+                            ? SizedBox(
+                                height: 30,
+                                width: 30,
+                                child: CircularProgressIndicator(
+                                  color: Colors.white,
+                                ),
+                              )
+                            : CupertinoButton(
+                                color: Colors.white,
+                                onPressed: () {
+                                  send();
+                                },
+                                child: Icon(
+                                  Icons.send,
+                                  color: Colors.black,
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              textStyle: GoogleFonts.inter(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-              isSender: model.role == "user",
-            );
-          },
-        ),
       ),
     );
   }
--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const baseUrl = "https://cors-anywhere.herokuapp.com/https://ai-chatbot-backend-k388.onrender.com/api";
+  static const baseUrl = "https://ai-chatbot-backend-k388.onrender.com/api";
 
   // auth
   static const google = "$baseUrl/auth/google";
