8a467209f1bc1f8ed26aba31d62e472c809fa139
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-22 17:59:48 +0500

    imp

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const baseUrl = "https://ai-chatbot-backend-k388.onrender.com/api";
+  static const baseUrl = "https://aichatbotbackend-production-ef4d.up.railway.app/api";
 
   // auth
   static const google = "$baseUrl/auth/google";
